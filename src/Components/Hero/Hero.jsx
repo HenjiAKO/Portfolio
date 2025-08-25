5f2c938c54f5b2a3cdd2cf3c51824f2c8df281ac
ToffieThee2 <kylemyrvin66@gmail.com>
2025-08-25 17:10:28 +0800

    chore: fix profile

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.jpg";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <img src={profile_img} alt="" />
-        <h1><span>I'm Henji,</span> a Second Year College Web Developer in Philippines</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis architecto eius quam a repudiandae libero cum pariatur eveniet culpa ut </p>
-        <div className="hero-action">
-            <div className="hero-connect">Connect with Me</div>
-            <div className="hero-resume">My resume</div>
-        </div>
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Henji,</span> a Second Year College Web Developer in
+        Philippines
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+        architecto eius quam a repudiandae libero cum pariatur eveniet culpa ut{" "}
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with Me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
