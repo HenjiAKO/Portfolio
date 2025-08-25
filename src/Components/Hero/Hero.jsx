1a3076d56b1ae1f83e7e57c36d736668c5544dca
ToffieThee2 <kylemyrvin66@gmail.com>
2025-08-25 15:56:28 +0800

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>I'm Henji,</span> a Second Year College Web Developer in Philippines</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis architecto eius quam a repudiandae libero cum pariatur eveniet culpa ut </p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with Me</div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
