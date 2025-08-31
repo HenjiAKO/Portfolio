344518b88e0b48d9929f5b2d8414a665430e904f
ToffieThee2 <kylemyrvin66@gmail.com>
2025-08-31 15:32:19 +0800

    add Projects & Services

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import Projects from "./Components/Projects/Projects";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
+      <Navbar/>
+      <Hero/>
       <About/>
+      <Services/>
+      <Projects/>
     </div>
   );
 };
---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Projects.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import arrow_icon from '../../assets/arrow_icon.svg'
+import projects_data from '../../assets/projects_data'
+
+
+const Project = () => {
+  return (
+    <div className='projects'>
+      <div className="projects-title">
+        <h1>My Projects</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="projects-container">
+        {projects_data.map((project, index)=>{
+          return <img key={index} src={project.p_img} alt=""/>
+        })}
+      </div>
+      <div className="projects-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Project
